5f920eff2a6765ec90cb45dc53ad40a11ab2b607
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-12 01:03:15 -0700

    connect with server

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jfs4t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri); const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('Hello from Doctor Portal!')
