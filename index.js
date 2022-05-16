d85be9c08a614679cd3d9edb01dfdfc0775d588f
tashfiaislamistila <tashfia.cse@gmail.com>
2022-05-15 21:32:35 -0700

    authorization of an admin

--- index.js
+++ index.js
@@ -52,14 +52,20 @@ async function run() {
 
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
         })
-
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
